b82653f9b0bd3bfb424695b998aa1fec25b63836
codedata <social@codedata.com.tw>
2014-02-17 11:34:53 +0800

    add exercise14

---
+++ exercises/exercise14/DVDLibrary/src/main/java/tw/codedata/DirectorDaoHibernateImpl.java
@@ -0,0 +1,44 @@
+package tw.codedata;
+
+import com.google.common.base.Optional;
+import java.util.List;
+import org.hibernate.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+// add annotation
+public class DirectorDaoHibernateImpl implements DirectorDao {
+    private SessionFactory sessionFactory;
+
+    // add annotation
+    public DirectorDaoHibernateImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+    
+    @Override
+    public void saveDirector(Director director) {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.save(director);
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    @Override
+    public Optional<Director> maybeFromName(String name) {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        
+        List<Director> directors = 
+                          session
+                          .createQuery("from Director as d where d.name = :name")
+                          .setString("name", name).list();
+        
+        session.getTransaction().commit();
+        session.close();
+        
+        return directors.isEmpty() ? Optional.<Director>absent() :  Optional.of(directors.get(0));
+        
+    }
+    
+}
---
+++ exercises/exercise14/DVDLibrary/src/main/java/tw/codedata/DvdDaoHibernateImpl.java
@@ -0,0 +1,40 @@
+package tw.codedata;
+
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+// add annotation
+public class DvdDaoHibernateImpl implements DvdDao {
+    private SessionFactory sessionFactory;
+
+    // add annotation
+    public DvdDaoHibernateImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public void saveDvd(Dvd dvd) {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.save(dvd);
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    @Override
+    public List<Dvd> allDvds() {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        
+         List<Dvd> dvds = session.createQuery("from Dvd").list();
+        
+        session.getTransaction().commit();
+        session.close();
+        
+        return dvds;
+    }
+
+}
